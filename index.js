af71fb074a6161e259383dae0e279f143e5508f3
MiliiCodes <milotka.stykova@gmail.com>
2022-04-22 17:38:31 +0200

    setting js functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("working")
+//zakladám premennú pre hráča, ktorý je na rade
+
+let player = "circle"
+
+//nastavujem funkciu, ktorá zmení obsah tlačítok a hráča, ktorý je na rade
+
+const fillButton = (event) => {
+    event.target.classList.add(`game__fields--${player}`)
+    if (player === "circle") {
+        player = "cross"
+    } else {
+        player = "circle"
+    }
+    document.querySelector("#player").src = `assets/${player}.svg`
+}
+
+
+//nastavujem funkciu, ktorá spustí pri kliknutí na tlačítko funkciu nastavenú vyššie
+
+const clickedButton = document.querySelectorAll(".game__fields--empty")
+for (let i = 0; i < clickedButton.length; i += 1) {
+    clickedButton[i].addEventListener("click", fillButton)
+}
+
