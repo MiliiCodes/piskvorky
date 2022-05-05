dae39e419207a6d675562d07e788212493bee9ff
MiliiCodes <milotka.stykova@gmail.com>
2022-05-05 11:37:33 +0200

    changing class names

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let player = "circle"
 //nastavujem funkciu, ktorá zmení obsah tlačítok a hráča, ktorý je na rade
 
 const fillButton = (event) => {
-    event.target.classList.add(`game__fields--${player}`)
+    event.target.classList.add(`game__field--${player}`)
     event.target.disabled = true
     if (player === "circle") {
         player = "cross"
@@ -19,15 +19,15 @@ const fillButton = (event) => {
 
 //nastavujem funkciu, ktorá spustí pri kliknutí na tlačítko funkciu nastavenú vyššie
 
-const clickedButton = document.querySelectorAll(".game__fields--empty")
+const clickedButton = document.querySelectorAll(".game__field")
 for (let i = 0; i < clickedButton.length; i += 1) {
     clickedButton[i].addEventListener("click", fillButton)
 }
 
 const getSymbol = (field) => {
-    if (field.classList.contains("game__fields--cross")) {
+    if (field.classList.contains("game__field--cross")) {
         return "cross"
-    } else if (field.classList.contains("game__fields--circle")) {
+    } else if (field.classList.contains("game__field--circle")) {
         return "circle"
     }
 }
