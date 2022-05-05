e7ebc5053a418d922c12dbe16a4881700aa8a82d
MiliiCodes <milotka.stykova@gmail.com>
2022-05-05 12:51:42 +0200

    adding js to recognize a winner

--- index.js
+++ index.js
@@ -11,11 +11,17 @@ const fillButton = (event) => {
         player = "cross"
     } else {
         player = "circle"
-    }
 
     document.querySelector("#player").src = `assets/${player}.svg`}
 
-
+    if (isWinningMove(event.target)) {
+        if (player === 'cross') {
+          confirm('Hráč, který měl kolečka, vyhrává!');
+        } else {
+          confirm('Hráč, který měl křížek vyhrává!');
+        }
+      }
+    };
 
 //nastavujem funkciu, ktorá spustí pri kliknutí na tlačítko funkciu nastavenú vyššie
 
@@ -25,10 +31,92 @@ for (let i = 0; i < clickedButton.length; i += 1) {
 }
 
 const getSymbol = (field) => {
-    if (field.classList.contains("game__field--cross")) {
-        return "cross"
-    } else if (field.classList.contains("game__field--circle")) {
-        return "circle"
-    }
+	// Název třídy přizpůsob tvému kódu.
+	if (field.classList.contains('game__field--cross')) {
+		return "cross"
+	} else if (field.classList.contains('game__field--circle')) {
+		return "circle"
+	}
+
 }
 
+const boardSize = 10 // 10x10
+
+const getField = (row, column) => {
+   return clickedButton[row * boardSize + column]
+}
+
+console.log(getField(1, 3));
+
+const getPosition = (field) => {
+	let fieldIndex = 0
+	while (fieldIndex < clickedButton.length && field !== clickedButton[fieldIndex]) {
+		fieldIndex++
+	}
+
+	return {
+		row: Math.floor(fieldIndex / boardSize),
+		column: fieldIndex % boardSize,
+	}
+}
+
+console.log(getPosition(1, 3));
+
+
+const symbolsToWin = 5
+const isWinningMove = (field) => {
+	const origin = getPosition(field)
+	const symbol = getSymbol(field)
+
+	let i
+
+	let inRow = 1 // Jednička pro právě vybrané políčko
+	// Koukni doleva
+	i = origin.column
+	while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+		inRow++
+		i--
+	}
+
+	// Koukni doprava
+	i = origin.column
+	while (
+		i < boardSize - 1 &&
+		symbol === getSymbol(getField(origin.row, i + 1))
+	) {
+		inRow++
+		i++
+	}
+
+	if (inRow >= symbolsToWin) {
+		return true
+	}
+
+	let inColumn = 1
+	// Koukni nahoru
+	i = origin.row
+	while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+		inColumn++
+		i--
+	}
+
+	// Koukni dolu
+	i = origin.row
+	while (
+		i < boardSize - 1 &&
+		symbol === getSymbol(getField(i + 1, origin.column))
+	) {
+		inColumn++
+		i++
+	}
+
+	if (inColumn >= symbolsToWin) {
+		return true
+	}
+
+	return false
+}
+
+for (let i = 0; i < clickedButton.length; i++) {
+    clickedButton[i].addEventListener('click', fillButton);
+  }
